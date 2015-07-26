cfae137df9d21fd210ff8c9986c590019adb766c
Shaw Innes <shaw@immortal.net.au>
2015-07-26 19:28:34 +1000

    Update Version

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,7 @@ maintainer       'Shawn Neal'
 maintainer_email 'sneal@daptiv.com'
 license          'Apache 2.0'
 description      'Installs/Configures Visual Studio'
-ver_path = File.join(File.dirname(__FILE__), 'version.txt')
-version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.0.3').chomp
+version          '0.0.4'
 
 depends          'windows'
 depends          'seven_zip'
