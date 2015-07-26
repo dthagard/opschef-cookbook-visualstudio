d8518cddbbcfefc852ee8256766f8ce0026f20bd
Shaw Innes <shaw@immortal.net.au>
2015-07-26 19:39:39 +1000

    Update Version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Shawn Neal'
 maintainer_email 'sneal@daptiv.com'
 license          'Apache 2.0'
 description      'Installs/Configures Visual Studio'
-version          '0.0.4'
+version          '0.0.5'
 
 depends          'windows'
 depends          'seven_zip'
